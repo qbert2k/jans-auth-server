594e678ad7fabf9bfba18ed471d8a9eab26c3f4e
SMan <sman@smansoft.com>
2021-07-27 11:51:40 +0300

    updating unit tests (CrossEncryptionTest): draft 13;

--- server/src/test/java/io/jans/as/server/comp/CrossEncryptionTest.java
+++ server/src/test/java/io/jans/as/server/comp/CrossEncryptionTest.java
@@ -11,13 +11,22 @@ import static org.testng.Assert.assertTrue;
 import static org.testng.AssertJUnit.assertNotNull;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.InvalidParameterException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.Security;
 import java.security.Signature;
+import java.security.SignatureException;
+import java.security.cert.CertificateEncodingException;
 import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.RSAPrivateKey;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.codec.Charsets;
@@ -60,6 +69,9 @@ import com.nimbusds.jwt.SignedJWT;
 
 import io.jans.as.model.configuration.AppConfiguration;
 import io.jans.as.model.crypto.AbstractCryptoProvider;
+import io.jans.as.model.crypto.Certificate;
+import io.jans.as.model.crypto.Key;
+import io.jans.as.model.crypto.KeyFactory;
 import io.jans.as.model.crypto.encryption.BlockEncryptionAlgorithm;
 import io.jans.as.model.crypto.encryption.KeyEncryptionAlgorithm;
 import io.jans.as.model.crypto.signature.RSAKeyFactory;
@@ -86,14 +98,15 @@ public class CrossEncryptionTest {
 		Security.addProvider(new BouncyCastleProvider());
 	}
 
-	// final String encryptedJweProducedByGluu =
-	// "eyJ0eXAiOiJKV1QiLCJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkExMjhHQ00iLCJraWQiOiIyIn0.M9YXhzGlMBxJRFjpIZ3ybfNODALPz_08WADIpWSLHOoCBdwqPWQ3fwDf-uaiw7wyTTf9piuKVUOeYHnPE6C_EmS9gj5fmckHBCHcNxZanobT0QXZdy-64wb4GK3ar66lPPFnJMVLLCqZfUjB1gHxmAcwrVJQTUPO0ogk2nZCujp4mOuJ0QnOQmJ0R1rHTjbYmKBDySIavmkXosoJaLZI4N1CltCKj66P_XKYLfgAE0yevuwtNxkkRc2EGMyPpZ8pVjBL5TPQF3b5AyAstUvB4l6o90JZQLzvAdHJyGuCW1zwzGPBtVBVYvb2vBBAuj7EPKDU9UQDuDoklwj5Hwc6wg.qBM-41MJ46_eUv4I.mak_e28_onSOODjdH06wWuA0MfJMTGConWSekPIArQoFKAgcxVRvg-JNqjaBFaG4ck8cp0ViAke_Cbfl4AyN-gAFI2pqEMiXkoEB193SyD6Yev0P1zKTJORWS6tpznYAGYgIPh_rWyWPFSdT1WPB7Qgzarf-JNYrNe5H_P8JRrArWyCEJx4w6_WLcGnM1EQQPkThoYC4utS47W0OHf2SNr-PRUhCeoEIuoMaQUmjYq386BjCWhQEoQCZNftUjUXZBq8MepW92v1spNLCb7NTEJ1p3s45KIVwPt5qnXI6-ouQE4_KFXVNe5-SSfyzrEf1jxTyerNqlU5bIZ0v4aPS6i3bXSSHIfgyvrFCzDPq9x-5B98OI0sVDKxzzp7UWjqEjjmuQbdN4eGZUtSGYcWNFI29vl4Pr8HvqMjnQaaEtGZeX_nJG27xzlwlD1pI_rjO_QMAQpfbNuxLm5-HhB0fZOngjNAnOhipyY_tTMMtiWLmoJUuicwTTSpERC_9ny8tnsiyCOEJEyeZFEzh52jfox_WHLVkIrjCUCYtTwCvuYdtu4Sgl-WPCa2y-4uF7u2DcIKdIRRMdjgE1RNUAp-W2ui8PDrIaSVxkWbuLQJ2oXEyWN8gFHEZPko-n80IjGG8Si3Qh1kum_vO9Ub7AiIm0pk65ph_CQH0BSVSLwN-e4iAd1C6h_J2O-aGEKWKrvvRC31ApCr5RkOdaKTAYVGUKQSMBdqucq47JbBynP7dqE0Kxl3miBo_dyYXCim9Gw.DSoXCEJ7-uT7Xv7eb3g-7A";
+	// final String encryptedJweProducedByGluu = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkExMjhHQ00iLCJraWQiOiIyIn0.M9YXhzGlMBxJRFjpIZ3ybfNODALPz_08WADIpWSLHOoCBdwqPWQ3fwDf-uaiw7wyTTf9piuKVUOeYHnPE6C_EmS9gj5fmckHBCHcNxZanobT0QXZdy-64wb4GK3ar66lPPFnJMVLLCqZfUjB1gHxmAcwrVJQTUPO0ogk2nZCujp4mOuJ0QnOQmJ0R1rHTjbYmKBDySIavmkXosoJaLZI4N1CltCKj66P_XKYLfgAE0yevuwtNxkkRc2EGMyPpZ8pVjBL5TPQF3b5AyAstUvB4l6o90JZQLzvAdHJyGuCW1zwzGPBtVBVYvb2vBBAuj7EPKDU9UQDuDoklwj5Hwc6wg.qBM-41MJ46_eUv4I.mak_e28_onSOODjdH06wWuA0MfJMTGConWSekPIArQoFKAgcxVRvg-JNqjaBFaG4ck8cp0ViAke_Cbfl4AyN-gAFI2pqEMiXkoEB193SyD6Yev0P1zKTJORWS6tpznYAGYgIPh_rWyWPFSdT1WPB7Qgzarf-JNYrNe5H_P8JRrArWyCEJx4w6_WLcGnM1EQQPkThoYC4utS47W0OHf2SNr-PRUhCeoEIuoMaQUmjYq386BjCWhQEoQCZNftUjUXZBq8MepW92v1spNLCb7NTEJ1p3s45KIVwPt5qnXI6-ouQE4_KFXVNe5-SSfyzrEf1jxTyerNqlU5bIZ0v4aPS6i3bXSSHIfgyvrFCzDPq9x-5B98OI0sVDKxzzp7UWjqEjjmuQbdN4eGZUtSGYcWNFI29vl4Pr8HvqMjnQaaEtGZeX_nJG27xzlwlD1pI_rjO_QMAQpfbNuxLm5-HhB0fZOngjNAnOhipyY_tTMMtiWLmoJUuicwTTSpERC_9ny8tnsiyCOEJEyeZFEzh52jfox_WHLVkIrjCUCYtTwCvuYdtu4Sgl-WPCa2y-4uF7u2DcIKdIRRMdjgE1RNUAp-W2ui8PDrIaSVxkWbuLQJ2oXEyWN8gFHEZPko-n80IjGG8Si3Qh1kum_vO9Ub7AiIm0pk65ph_CQH0BSVSLwN-e4iAd1C6h_J2O-aGEKWKrvvRC31ApCr5RkOdaKTAYVGUKQSMBdqucq47JbBynP7dqE0Kxl3miBo_dyYXCim9Gw.DSoXCEJ7-uT7Xv7eb3g-7A";
 	private final String encryptedJweProducedByGluu = "eyJraWQiOiIxIiwidHlwIjoiSldUIiwiZW5jIjoiQTEyOEdDTSIsImFsZyI6IlJTQS1PQUVQIn0.bnWzspu4G40jEAkOjV-yRsXnHhgy7MgHxDKHc_ePWqCji-rNfeViybYV62jSCGRWsRB1sGiLuiE35z8aag4dr1gIbYARfNB7t9kaBcZCfZ-jwaGUYn-XfCDg98U4VVv1P77R8Gu-OcU53vBM_pPCzOm75IelWf_W8wFK4DB6i9P8CDFVlsDSWslMfqsMZLj9lE0KV_10c2ovELzcTu-GPC-rMUglFSHIt8Povi7bFf-kiWxFd1kT0NdrnHmKUVqIRNv5fsAtbY5B7jx5-EQ_IjhdaoK0QwfaqF0Vz4qVOO7y1PSXdDXyvrLwSY8rrTjzaLbXCnLc9oLeiIP-aR3HuA.YB2_esWvrHdJh1jt.P56SeJfBlBDm73YVQsEH_8ZtBgwQpnpX0hKY7v2ufFuqAlP2BeR2Ku-3rgIhFHPOAhqRuZ-YOROwIUVfC9ceG0tI63W_Xf0.FyuoL4LlnBvPEnmCJ5H8pw";
 
 	private final String senderJwkJson = "{\"kty\":\"RSA\",\"d\":\"iSx-zxihgOITpEhz6WwGiiCZjxx597wqblhSYgFWa_bL9esLY3FT_Kq9sdvGPiI8QmObRxPZuTi4n3BVKYUWcfjVz3swq7VmESxnJJZE-vMI9NTaZ-CT2b4I-c3qwAsejhWagJf899I3MRtPOnyxMimyOw4_5YYvXjBkXkCMfCsbj5TBR3RbtMrUYzDMXsVT1EJ_7H76DPBFJx5JptsEAA17VMtqwvWhRutnPyQOftDGPxD-1aGgpteKOUCv7Lx-mFX-zV6nnPB8vmgTgaMqCbCFKSZI567p714gzWBkwnNdRHleX8wos8yZAGbdwGqqUz5x3iKKdn3c7U9TTU7DAQ\",\"e\":\"AQAB\",\"use\":\"sig\",\"kid\":\"1\",\"alg\":\"RS256\",\"n\":\"i6tdK2fREwykTUU-qkYkiSHgg9B31-8EjVCbH0iyrewY9s7_WYPT7I3argjcmiDkufnVfGGW0FadtO3br-Qgk_N2e9LqGMtjUoGMZKFS3fJhqjnLYDi_E5l2FYU_ilw4EXPsZJY0CaM7BxjwUBoCjopYrgvtdxA9G6gpGoAH4LopAkgX-gkawVLpB4NpLvA09FLF2OlYZL7aaybvM2Lz_IXEPa-LSOwLum80Et-_A1-YMx_Z767Iwl1pGTpgZ87jrDD1vEdMdiLcWFG3UIYAAIxtg6X23cvQVLMaXKpyV0USDCWRJrZYxEDgZngbDRj3Sd2-LnixPkMWAfo_D9lBVQ\"}";
-
 	private final String recipientJwkJson = "{\"kty\":\"RSA\",\"d\":\"jAFM0c4oXxh5YcEujZRVY5LNUzkm0OZf8OUZ31DockQE07BwSAsi4_y6vursS4Z74EurjYlfPx7WoZZokTLyBReVvG8XQZ-AQ5smU9gXQrsiVdU2kOp17oYnOP3OKc0HtvlfTPKdz0DhoA--wAsPFCL2ei4Qly_J3IQTF9ffJJMEyzgabcV1xqrk8NEK5XfEHOdNHzzg-doRe4lCsDcEfIppCIxPHTozhYpwH0_OrssAX1OwX5Jx6-5pXc_BIBrymIkjfwlPYBC32f0iD6VTntJfIngMOdeu0t6krOaWlbfmf6RdoM5sugT-j3mYnd3w4c2eFW23Z9sPCrQvDNlTcQ\",\"e\":\"AQAB\",\"use\":\"enc\",\"kid\":\"2\",\"alg\":\"RS256\",\"n\":\"oaPsFKHgVnK0d04rjN5GgZFqCh9HwYkLMdDQDIgkM3x4sxTpctS5NJQK7iKWNxPTtULdzrY6NLqtrNWmIrJFC6f2h4q5p46Kmc8vdhm_Ph_jpYfsXWTdsHAoee6iJPMoie7rBGoscr3y2DdNlyxAO_jHLUkaaSAqDQrH_f4zVTO0XKisJu8DxKoh2U8myOow_kxx4PUxEdlH6XclpxYT5lIZijOZ8wehFad_BAJ2iZM40JDoqOgspUF1Jyq7FjOoMQabYYwDMyfs2rEALcTU1UsvLeWbl95T3mdAw64Ux3uFCZzHdXF4IDr7xH4NrEVT7SMAlwNoaRfmFbtL-WoISw\"}";
+	
 	public static final String PAYLOAD = "{\"iss\":\"https:devgluu.saminet.local\",\"sub\":\"testing\"}";
+	
+	private final String rsaJwkJson = "{ \"kty\":\"RSA\", \"d\":\"BSOr_bbK0THvHyqE8CaPE-f26VBUFRqry47VW0MWtZyU7tGWoBNJi-hB4kxDskw7HitOpdx2zXDhQq6rg6Yv1Wn4WTHSFtQ1_vEJaCOunN1SejrhfEz1eFrADuCyOUXflrUduhymvWGltIgd000kib3QvwvsIft597wqfW5kDds_JgnTILnk-UkKdVx58SGhkgkcUI8uo9BSN5MMGgqEUHY_orqMa-oVWy5VsOAMU22m2ZuPhEZa4uOH4xCRkRtid7LlgzWCOncAAhM4trspKM60Zntq1m6D7ir_bDD1qxpEWYOvEH1UhvUcQw8UKYxGZ3VwscSBlTxQ15mNiDi-wQ\", \"e\":\"AQAB\", \"use\":\"enc\", \"use\":\"sig\", \"kid\":\"1\", \"alg\":\"RS256\", \"n\":\"ALPsdlfm6QYerQFRhL2-QOJUn-a70JsuElspspyBXRZUllhpxSaNWZlouT0DAf3NUmEm8wgVbJF1-exMVaaF8kUrdGYKS5N1uDnZwc1G8MIt29YQ6xoLLqzihJlD-syYL6tPYCr97a4AbA8EssrMjynk_WIBh18gRESq-I9vyvlYyjRfZ9ey185ERGpMfm0d4Mouttl2nc3VRfuuLIstQ4ylKvMjnWtuAvBdwF7jJSIalDUjxIA9kkg6dH2e0ZNyDlN8-14VkeAaC73f-va0kBrD9bPIUSuaaIkmgN0lKwR_xH1Y7vG2xs45R1d_cVTSo-yXhPgKLphPXflPkMMXBs0\" }";	
 
 	private final String ecJwkJson = "{ \"kty\":\"EC\", \"crv\":\"P-256\", \"x\":\"MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4\", \"y\":\"4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM\", \"d\":\"870MB6gfuTJ4HtUnUvYMyJpr5eUZNP4Bk43bVdj3eAE\", \"use\":\"enc\", \"kid\":\"3\" }";
 
@@ -153,9 +166,9 @@ public class CrossEncryptionTest {
 	};	
 	
 	KeyEncryptionAlgorithmSuite[] keyEnrAlgorithmsRSA = {
-			new KeyEncryptionAlgorithmSuite (KeyEncryptionAlgorithm.RSA1_5, recipientJwkJson, null),
-			new KeyEncryptionAlgorithmSuite (KeyEncryptionAlgorithm.RSA_OAEP, recipientJwkJson, null),
-			new KeyEncryptionAlgorithmSuite (KeyEncryptionAlgorithm.RSA_OAEP_256, recipientJwkJson, null),
+			new KeyEncryptionAlgorithmSuite (KeyEncryptionAlgorithm.RSA1_5, rsaJwkJson, null),
+			new KeyEncryptionAlgorithmSuite (KeyEncryptionAlgorithm.RSA_OAEP, rsaJwkJson, null),
+			new KeyEncryptionAlgorithmSuite (KeyEncryptionAlgorithm.RSA_OAEP_256, rsaJwkJson, null),
 	};
 
 	KeyEncryptionAlgorithmSuite[] keyEnrAlgorithmsECDH = {
@@ -180,6 +193,113 @@ public class CrossEncryptionTest {
 			new KeyEncryptionAlgorithmSuite (KeyEncryptionAlgorithm.PBES2_HS512_PLUS_A256KW, passwordValue, null)
 	};
 
+	@Test
+    public void getKeys() {
+    	
+		try {
+	    	int keyLength;
+	    	final String passwMessage = "Some Password";
+	    	MessageDigest sha512;
+			sha512 = MessageDigest.getInstance("SHA-512");
+			
+	    	byte[] sha512Array = sha512.digest(passwMessage.getBytes());
+	    	
+	    	keyLength = 128 / 8;
+	    	byte[] sha512Array_16 = Arrays.copyOf(sha512Array, keyLength);
+
+	    	keyLength = 192 / 8;
+	    	byte[] sha512Array_24 = Arrays.copyOf(sha512Array, keyLength);
+	    	
+	    	keyLength = 256 / 8;
+	    	byte[] sha512Array_32 = Arrays.copyOf(sha512Array, keyLength);
+	    	
+	    	keyLength = 384 / 8;
+	    	byte[] sha512Array_48 = Arrays.copyOf(sha512Array, keyLength);
+	    	
+	    	keyLength = 512 / 8;
+	    	byte[] sha512Array_64 = Arrays.copyOf(sha512Array, keyLength);
+	    	
+	    	String sha512Str_16 = Base64Util.base64urlencode(sha512Array_16);
+	    	String sha512Str_24 = Base64Util.base64urlencode(sha512Array_24);	    	
+	    	String sha512Str_32 = Base64Util.base64urlencode(sha512Array_32);
+	    	String sha512Str_48 = Base64Util.base64urlencode(sha512Array_48);	    	
+	    	String sha512Str_64 = Base64Util.base64urlencode(sha512Array_64);	    	
+	    	
+	        System.out.println("sha512Str_16 = " + sha512Str_16);
+	        System.out.println("sha512Str_24 = " + sha512Str_24);
+	        System.out.println("sha512Str_32 = " + sha512Str_32);	        
+	        System.out.println("sha512Str_48 = " + sha512Str_48);	        
+	        System.out.println("sha512Str_64 = " + sha512Str_64);	        
+	    	
+	        byte[] sha512Array_16_dec = Base64Util.base64urldecode(sha512Str_16);
+	        byte[] sha512Array_24_dec = Base64Util.base64urldecode(sha512Str_24);	        
+	        byte[] sha512Array_32_dec = Base64Util.base64urldecode(sha512Str_32);
+	        byte[] sha512Array_48_dec = Base64Util.base64urldecode(sha512Str_48);	        
+	        byte[] sha512Array_64_dec = Base64Util.base64urldecode(sha512Str_64);	        
+	        
+	        assertTrue(Arrays.equals(sha512Array_16, sha512Array_16_dec));
+	        assertTrue(Arrays.equals(sha512Array_24, sha512Array_24_dec));
+	        assertTrue(Arrays.equals(sha512Array_32, sha512Array_32_dec));
+	        assertTrue(Arrays.equals(sha512Array_48, sha512Array_48_dec));	        
+	        assertTrue(Arrays.equals(sha512Array_64, sha512Array_64_dec));
+	        
+/*	        
+	        import io.jans.as.model.crypto.signature.ECDSAKeyFactory;
+	        import io.jans.as.model.crypto.signature.ECDSAPrivateKey;
+	        import io.jans.as.model.crypto.signature.ECDSAPublicKey;
+	        import io.jans.as.model.crypto.signature.EDDSAKeyFactory;
+	        import io.jans.as.model.crypto.signature.EDDSAPrivateKey;
+	        import io.jans.as.model.crypto.signature.EDDSAPublicKey;
+	        import io.jans.as.model.crypto.signature.RSAKeyFactory;
+	        import io.jans.as.model.crypto.signature.RSAPrivateKey;
+	        import io.jans.as.model.crypto.signature.RSAPublicKey;
+*/	        	        
+	        
+	        io.jans.as.model.crypto.KeyFactory<io.jans.as.model.crypto.signature.RSAPrivateKey, 
+	        io.jans.as.model.crypto.signature.RSAPublicKey> keyFactory = new io.jans.as.model.crypto.signature.RSAKeyFactory(SignatureAlgorithm.RS256,
+	        		"CN=Test CA Certificate");
+
+			Key<io.jans.as.model.crypto.signature.RSAPrivateKey, io.jans.as.model.crypto.signature.RSAPublicKey> key = keyFactory.getKey();
+
+			io.jans.as.model.crypto.signature.RSAPrivateKey privateKey = key.getPrivateKey();
+			io.jans.as.model.crypto.signature.RSAPublicKey publicKey = key.getPublicKey();
+			io.jans.as.model.crypto.Certificate certificate = key.getCertificate();
+
+			BigInteger dPriv = privateKey.getPrivateExponent();
+			BigInteger nPriv = privateKey.getModulus();
+			
+			String dPrivStr = Base64Util.base64urlencode(dPriv.toByteArray());
+			String nPrivStr = Base64Util.base64urlencode(nPriv.toByteArray());
+
+			BigInteger dPub = publicKey.getPublicExponent();
+			BigInteger nPub = publicKey.getModulus();
+			
+			String dPubStr = Base64Util.base64urlencode(dPub.toByteArray());
+			String nPubStr = Base64Util.base64urlencode(nPub.toByteArray());
+			
+	        System.out.println("dPrivStr = " + dPrivStr);
+	        System.out.println("nPrivStr = " + nPrivStr);
+	        
+	        System.out.println("dPubStr = " + dPubStr);
+	        System.out.println("nPubStr = " + nPubStr);
+			
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidParameterException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (CertificateEncodingException e) {
+			e.printStackTrace();
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		} catch (SignatureException e) {
+			e.printStackTrace();
+		}
+    	
+        assertTrue(true);
+    }
+	
 
 	@Test
 	public void encryptWithNimbus_decryptByAll() {
@@ -471,6 +591,17 @@ public class CrossEncryptionTest {
 					final String decryptedPayload = decrypter.decrypt(jweStr).getClaims().toJsonString().toString();
 					
 					assertTrue(isJsonEqual(decryptedPayload, PAYLOAD));
+					
+					RSAPrivateKey rsaPrivateKeyWrong = ((RSAKey) JWK.parse(rsaJwkJson)).toRSAPrivateKey();
+					
+					decrypter = new JweDecrypterImpl(rsaPrivateKeyWrong);
+
+					decrypter.setKeyEncryptionAlgorithm(keyEncryptionAlgorithm);
+					decrypter.setBlockEncryptionAlgorithm(blockEncryptionAlgorithm);
+					final String decryptedPayload1 = decrypter.decrypt(jweStr).getClaims().toJsonString().toString();
+
+					assertTrue(isJsonEqual(decryptedPayload1, PAYLOAD));
+					
 				} catch (Exception e) {
 					System.out.println("Error (encryptWithGluu_RSA_decryptByAll) : " +
 							" blckEncrAlgorithm = " + blckEncrAlgorithm +
