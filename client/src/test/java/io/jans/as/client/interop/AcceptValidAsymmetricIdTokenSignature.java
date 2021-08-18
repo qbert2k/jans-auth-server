a64c99115214d4ed8f39bd314088865a650c5d93
SMan <sman@smansoft.com>
2021-08-18 18:09:22 +0300

    extending crypto support; business-level, controller: draft 31; Working signature tests (AcceptValidAsymmetricIdTokenSignature, UsesAsymmetricIdTokenSignatures): ES256K, ED25519, ED448;

--- client/src/test/java/io/jans/as/client/interop/AcceptValidAsymmetricIdTokenSignature.java
+++ client/src/test/java/io/jans/as/client/interop/AcceptValidAsymmetricIdTokenSignature.java
@@ -17,9 +17,11 @@ import io.jans.as.model.common.GrantType;
 import io.jans.as.model.common.ResponseType;
 import io.jans.as.model.common.SubjectType;
 import io.jans.as.model.crypto.signature.ECDSAPublicKey;
+import io.jans.as.model.crypto.signature.EDDSAPublicKey;
 import io.jans.as.model.crypto.signature.RSAPublicKey;
 import io.jans.as.model.crypto.signature.SignatureAlgorithm;
 import io.jans.as.model.jws.ECDSASigner;
+import io.jans.as.model.jws.EDDSASigner;
 import io.jans.as.model.jws.RSASigner;
 import io.jans.as.model.jwt.Jwt;
 import io.jans.as.model.jwt.JwtHeaderName;
@@ -96,6 +98,9 @@ public class AcceptValidAsymmetricIdTokenSignature extends BaseTest {
         RSAPublicKey publicKey = JwkClient.getRSAPublicKey(
                 jwksUri,
                 jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+        
+        System.out.println("rs256: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));        
+        
         RSASigner rsaSigner = new RSASigner(SignatureAlgorithm.RS256, publicKey);
         assertTrue(rsaSigner.validate(jwt));
     }
@@ -161,7 +166,218 @@ public class AcceptValidAsymmetricIdTokenSignature extends BaseTest {
         ECDSAPublicKey publicKey = JwkClient.getECDSAPublicKey(
                 jwksUri,
                 jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+        
+        System.out.println("es256: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));        
+        
         ECDSASigner ecdsaSigner = new ECDSASigner(SignatureAlgorithm.ES256, publicKey);
         assertTrue(ecdsaSigner.validate(jwt));
     }
+    
+
+    @Parameters({"redirectUris", "userId", "userSecret", "redirectUri", "postLogoutRedirectUri", "clientJwksUri"})
+    @Test
+    public void acceptValidAsymmetricIdTokenSignatureES256K(
+            final String redirectUris, final String userId, final String userSecret, final String redirectUri,
+            final String postLogoutRedirectUri, final String clientJwksUri) throws Exception {
+        showTitle("OC5:FeatureTest-Accept Valid Asymmetric ID Token Signature es256k");
+
+        List<ResponseType> responseTypes = Arrays.asList(
+                ResponseType.CODE,
+                ResponseType.ID_TOKEN);
+
+        List<GrantType> grantTypes = Arrays.asList(GrantType.AUTHORIZATION_CODE);
+
+        // 1. Registration
+        RegisterRequest registerRequest = new RegisterRequest(ApplicationType.WEB, null,
+                StringUtils.spaceSeparatedToList(redirectUris));
+        registerRequest.setResponseTypes(responseTypes);
+        registerRequest.setIdTokenSignedResponseAlg(SignatureAlgorithm.ES256K);
+        registerRequest.setPostLogoutRedirectUris(StringUtils.spaceSeparatedToList(postLogoutRedirectUri));
+        registerRequest.setJwksUri(clientJwksUri);
+        registerRequest.setSubjectType(SubjectType.PUBLIC);
+        registerRequest.setRequireAuthTime(true);
+        registerRequest.setDefaultMaxAge(3600);
+        registerRequest.setGrantTypes(grantTypes);
+
+        RegisterClient registerClient = new RegisterClient(registrationEndpoint);
+        registerClient.setRequest(registerRequest);
+        RegisterResponse registerResponse = registerClient.exec();
+
+        showClient(registerClient);
+        assertEquals(registerResponse.getStatus(), 201, "Unexpected response code: " + registerResponse.getEntity());
+        assertNotNull(registerResponse.getClientId());
+        assertNotNull(registerResponse.getClientSecret());
+        assertNotNull(registerResponse.getRegistrationAccessToken());
+        assertNotNull(registerResponse.getClientSecretExpiresAt());
+
+        String clientId = registerResponse.getClientId();
+
+        // 2. Request Authorization
+        List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
+        String nonce = UUID.randomUUID().toString();
+        String state = UUID.randomUUID().toString();
+
+        AuthorizationRequest authorizationRequest = new AuthorizationRequest(responseTypes, clientId, scopes, redirectUri, nonce);
+        authorizationRequest.setState(state);
+
+        AuthorizationResponse authorizationResponse = authenticateResourceOwnerAndGrantAccess(
+                authorizationEndpoint, authorizationRequest, userId, userSecret);
+
+        assertNotNull(authorizationResponse.getLocation());
+        assertNotNull(authorizationResponse.getIdToken());
+        assertNotNull(authorizationResponse.getState());
+        assertEquals(authorizationResponse.getState(), state);
+
+        String idToken = authorizationResponse.getIdToken();
+
+        // 3. Validate id_token
+        Jwt jwt = Jwt.parse(idToken);
+        ECDSAPublicKey publicKey = JwkClient.getECDSAPublicKey(
+                jwksUri,
+                jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+        
+        System.out.println("es256k: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));        
+        
+        ECDSASigner ecdsaSigner = new ECDSASigner(SignatureAlgorithm.ES256K, publicKey);
+        assertTrue(ecdsaSigner.validate(jwt));
+    }
+    
+
+    @Parameters({"redirectUris", "userId", "userSecret", "redirectUri", "postLogoutRedirectUri", "clientJwksUri"})
+    @Test
+    public void acceptValidAsymmetricIdTokenSignatureED25519(
+            final String redirectUris, final String userId, final String userSecret, final String redirectUri,
+            final String postLogoutRedirectUri, final String clientJwksUri) throws Exception {
+        showTitle("OC5:FeatureTest-Accept Valid Asymmetric ID Token Signature ed25519");
+
+        List<ResponseType> responseTypes = Arrays.asList(
+                ResponseType.CODE,
+                ResponseType.ID_TOKEN);
+
+        List<GrantType> grantTypes = Arrays.asList(GrantType.AUTHORIZATION_CODE);
+
+        // 1. Registration
+        RegisterRequest registerRequest = new RegisterRequest(ApplicationType.WEB, null,
+                StringUtils.spaceSeparatedToList(redirectUris));
+        registerRequest.setResponseTypes(responseTypes);
+        registerRequest.setIdTokenSignedResponseAlg(SignatureAlgorithm.ED25519);
+        registerRequest.setPostLogoutRedirectUris(StringUtils.spaceSeparatedToList(postLogoutRedirectUri));
+        registerRequest.setJwksUri(clientJwksUri);
+        registerRequest.setSubjectType(SubjectType.PUBLIC);
+        registerRequest.setRequireAuthTime(true);
+        registerRequest.setDefaultMaxAge(3600);
+        registerRequest.setGrantTypes(grantTypes);
+
+        RegisterClient registerClient = new RegisterClient(registrationEndpoint);
+        registerClient.setRequest(registerRequest);
+        RegisterResponse registerResponse = registerClient.exec();
+
+        showClient(registerClient);
+        assertEquals(registerResponse.getStatus(), 201, "Unexpected response code: " + registerResponse.getEntity());
+        assertNotNull(registerResponse.getClientId());
+        assertNotNull(registerResponse.getClientSecret());
+        assertNotNull(registerResponse.getRegistrationAccessToken());
+        assertNotNull(registerResponse.getClientSecretExpiresAt());
+
+        String clientId = registerResponse.getClientId();
+
+        // 2. Request Authorization
+        List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
+        String nonce = UUID.randomUUID().toString();
+        String state = UUID.randomUUID().toString();
+
+        AuthorizationRequest authorizationRequest = new AuthorizationRequest(responseTypes, clientId, scopes, redirectUri, nonce);
+        authorizationRequest.setState(state);
+
+        AuthorizationResponse authorizationResponse = authenticateResourceOwnerAndGrantAccess(
+                authorizationEndpoint, authorizationRequest, userId, userSecret);
+
+        assertNotNull(authorizationResponse.getLocation());
+        assertNotNull(authorizationResponse.getIdToken());
+        assertNotNull(authorizationResponse.getState());
+        assertEquals(authorizationResponse.getState(), state);
+
+        String idToken = authorizationResponse.getIdToken();
+
+        // 3. Validate id_token
+        Jwt jwt = Jwt.parse(idToken);
+        EDDSAPublicKey publicKey = JwkClient.getEDDSAPublicKey(
+                jwksUri,
+                jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+
+        System.out.println("ed25519: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+        
+        EDDSASigner ecdsaSigner = new EDDSASigner(SignatureAlgorithm.ED25519, publicKey);
+        assertTrue(ecdsaSigner.validate(jwt));
+    }        
+ 
+    
+    @Parameters({"redirectUris", "userId", "userSecret", "redirectUri", "postLogoutRedirectUri", "clientJwksUri"})
+    @Test
+    public void acceptValidAsymmetricIdTokenSignatureED448(
+            final String redirectUris, final String userId, final String userSecret, final String redirectUri,
+            final String postLogoutRedirectUri, final String clientJwksUri) throws Exception {
+        showTitle("OC5:FeatureTest-Accept Valid Asymmetric ID Token Signature ed448");
+
+        List<ResponseType> responseTypes = Arrays.asList(
+                ResponseType.CODE,
+                ResponseType.ID_TOKEN);
+
+        List<GrantType> grantTypes = Arrays.asList(GrantType.AUTHORIZATION_CODE);
+
+        // 1. Registration
+        RegisterRequest registerRequest = new RegisterRequest(ApplicationType.WEB, null,
+                StringUtils.spaceSeparatedToList(redirectUris));
+        registerRequest.setResponseTypes(responseTypes);
+        registerRequest.setIdTokenSignedResponseAlg(SignatureAlgorithm.ED448);
+        registerRequest.setPostLogoutRedirectUris(StringUtils.spaceSeparatedToList(postLogoutRedirectUri));
+        registerRequest.setJwksUri(clientJwksUri);
+        registerRequest.setSubjectType(SubjectType.PUBLIC);
+        registerRequest.setRequireAuthTime(true);
+        registerRequest.setDefaultMaxAge(3600);
+        registerRequest.setGrantTypes(grantTypes);
+
+        RegisterClient registerClient = new RegisterClient(registrationEndpoint);
+        registerClient.setRequest(registerRequest);
+        RegisterResponse registerResponse = registerClient.exec();
+
+        showClient(registerClient);
+        assertEquals(registerResponse.getStatus(), 201, "Unexpected response code: " + registerResponse.getEntity());
+        assertNotNull(registerResponse.getClientId());
+        assertNotNull(registerResponse.getClientSecret());
+        assertNotNull(registerResponse.getRegistrationAccessToken());
+        assertNotNull(registerResponse.getClientSecretExpiresAt());
+
+        String clientId = registerResponse.getClientId();
+
+        // 2. Request Authorization
+        List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
+        String nonce = UUID.randomUUID().toString();
+        String state = UUID.randomUUID().toString();
+
+        AuthorizationRequest authorizationRequest = new AuthorizationRequest(responseTypes, clientId, scopes, redirectUri, nonce);
+        authorizationRequest.setState(state);
+
+        AuthorizationResponse authorizationResponse = authenticateResourceOwnerAndGrantAccess(
+                authorizationEndpoint, authorizationRequest, userId, userSecret);
+
+        assertNotNull(authorizationResponse.getLocation());
+        assertNotNull(authorizationResponse.getIdToken());
+        assertNotNull(authorizationResponse.getState());
+        assertEquals(authorizationResponse.getState(), state);
+
+        String idToken = authorizationResponse.getIdToken();
+
+        // 3. Validate id_token
+        Jwt jwt = Jwt.parse(idToken);
+        EDDSAPublicKey publicKey = JwkClient.getEDDSAPublicKey(
+                jwksUri,
+                jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+
+        System.out.println("ed448: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));        
+        
+        EDDSASigner ecdsaSigner = new EDDSASigner(SignatureAlgorithm.ED448, publicKey);
+        assertTrue(ecdsaSigner.validate(jwt));
+    }        
+    
 }
--- client/src/test/java/io/jans/as/client/interop/UsesAsymmetricIdTokenSignatures.java
+++ client/src/test/java/io/jans/as/client/interop/UsesAsymmetricIdTokenSignatures.java
@@ -26,9 +26,11 @@ import io.jans.as.client.RegisterRequest;
 import io.jans.as.client.RegisterResponse;
 import io.jans.as.model.common.ResponseType;
 import io.jans.as.model.crypto.signature.ECDSAPublicKey;
+import io.jans.as.model.crypto.signature.EDDSAPublicKey;
 import io.jans.as.model.crypto.signature.RSAPublicKey;
 import io.jans.as.model.crypto.signature.SignatureAlgorithm;
 import io.jans.as.model.jws.ECDSASigner;
+import io.jans.as.model.jws.EDDSASigner;
 import io.jans.as.model.jws.RSASigner;
 import io.jans.as.model.jwt.Jwt;
 import io.jans.as.model.jwt.JwtHeaderName;
@@ -94,6 +96,9 @@ public class UsesAsymmetricIdTokenSignatures extends BaseTest {
         RSAPublicKey publicKey = JwkClient.getRSAPublicKey(
                 jwksUri,
                 jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+        
+        System.out.println("rs256: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));        
+        
         RSASigner rsaSigner = new RSASigner(SignatureAlgorithm.RS256, publicKey);
         assertTrue(rsaSigner.validate(jwt));
     }
@@ -149,6 +154,9 @@ public class UsesAsymmetricIdTokenSignatures extends BaseTest {
         RSAPublicKey publicKey = JwkClient.getRSAPublicKey(
                 jwksUri,
                 jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+        
+        System.out.println("rs384: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));          
+        
         RSASigner rsaSigner = new RSASigner(SignatureAlgorithm.RS384, publicKey);
         assertTrue(rsaSigner.validate(jwt));
     }
@@ -204,6 +212,9 @@ public class UsesAsymmetricIdTokenSignatures extends BaseTest {
         RSAPublicKey publicKey = JwkClient.getRSAPublicKey(
                 jwksUri,
                 jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+        
+        System.out.println("rs512: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));        
+        
         RSASigner rsaSigner = new RSASigner(SignatureAlgorithm.RS512, publicKey);
         assertTrue(rsaSigner.validate(jwt));
     }
@@ -259,10 +270,71 @@ public class UsesAsymmetricIdTokenSignatures extends BaseTest {
         ECDSAPublicKey publicKey = JwkClient.getECDSAPublicKey(
                 jwksUri,
                 jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+
+        System.out.println("es256: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));        
+        
         ECDSASigner ecdsaSigner = new ECDSASigner(SignatureAlgorithm.ES256, publicKey);
         assertTrue(ecdsaSigner.validate(jwt));
     }
 
+    @Parameters({"redirectUris", "userId", "userSecret", "redirectUri", "sectorIdentifierUri"})
+    @Test
+    public void usesAsymmetricIdTokenSignaturesES256K(
+            final String redirectUris, final String userId, final String userSecret, final String redirectUri,
+            final String sectorIdentifierUri) throws Exception {
+        showTitle("OC5:FeatureTest-Uses Asymmetric ID Token Signatures ES256K");
+
+        List<ResponseType> responseTypes = Arrays.asList(ResponseType.ID_TOKEN);
+
+        // 1. Registration
+        RegisterRequest registerRequest = new RegisterRequest(ApplicationType.WEB, "jans test app",
+                StringUtils.spaceSeparatedToList(redirectUris));
+        registerRequest.setResponseTypes(responseTypes);
+        registerRequest.setIdTokenSignedResponseAlg(SignatureAlgorithm.ES256K);
+        registerRequest.setSectorIdentifierUri(sectorIdentifierUri);
+
+        RegisterClient registerClient = new RegisterClient(registrationEndpoint);
+        registerClient.setRequest(registerRequest);
+        RegisterResponse registerResponse = registerClient.exec();
+
+        showClient(registerClient);
+        assertEquals(registerResponse.getStatus(), 201, "Unexpected response code: " + registerResponse.getEntity());
+        assertNotNull(registerResponse.getClientId());
+        assertNotNull(registerResponse.getClientSecret());
+        assertNotNull(registerResponse.getRegistrationAccessToken());
+        assertNotNull(registerResponse.getClientSecretExpiresAt());
+
+        String clientId = registerResponse.getClientId();
+
+        // 2. Request Authorization
+        List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
+        String nonce = UUID.randomUUID().toString();
+        String state = UUID.randomUUID().toString();
+
+        AuthorizationRequest authorizationRequest = new AuthorizationRequest(responseTypes, clientId, scopes, redirectUri, nonce);
+        authorizationRequest.setState(state);
+
+        AuthorizationResponse authorizationResponse = authenticateResourceOwnerAndGrantAccess(
+                authorizationEndpoint, authorizationRequest, userId, userSecret);
+
+        assertNotNull(authorizationResponse.getLocation());
+        assertNotNull(authorizationResponse.getIdToken());
+        assertNotNull(authorizationResponse.getState());
+
+        String idToken = authorizationResponse.getIdToken();
+
+        // 3. Validate id_token
+        Jwt jwt = Jwt.parse(idToken);
+        ECDSAPublicKey publicKey = JwkClient.getECDSAPublicKey(
+                jwksUri,
+                jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+        
+        System.out.println("es256k: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));        
+        
+        ECDSASigner ecdsaSigner = new ECDSASigner(SignatureAlgorithm.ES256K, publicKey);
+        assertTrue(ecdsaSigner.validate(jwt));
+    }
+    
     @Parameters({"redirectUris", "userId", "userSecret", "redirectUri", "sectorIdentifierUri"})
     @Test
     public void usesAsymmetricIdTokenSignaturesES384(
@@ -314,6 +386,9 @@ public class UsesAsymmetricIdTokenSignatures extends BaseTest {
         ECDSAPublicKey publicKey = JwkClient.getECDSAPublicKey(
                 jwksUri,
                 jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+        
+        System.out.println("es384: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));        
+        
         ECDSASigner ecdsaSigner = new ECDSASigner(SignatureAlgorithm.ES384, publicKey);
         assertTrue(ecdsaSigner.validate(jwt));
     }
@@ -369,7 +444,127 @@ public class UsesAsymmetricIdTokenSignatures extends BaseTest {
         ECDSAPublicKey publicKey = JwkClient.getECDSAPublicKey(
                 jwksUri,
                 jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+        
+        System.out.println("es512: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));        
+        
         ECDSASigner ecdsaSigner = new ECDSASigner(SignatureAlgorithm.ES512, publicKey);
         assertTrue(ecdsaSigner.validate(jwt));
     }
+    
+    @Parameters({"redirectUris", "userId", "userSecret", "redirectUri", "sectorIdentifierUri"})
+    @Test
+    public void usesAsymmetricIdTokenSignaturesED25519(
+            final String redirectUris, final String userId, final String userSecret, final String redirectUri,
+            final String sectorIdentifierUri) throws Exception {
+        showTitle("OC5:FeatureTest-Uses Asymmetric ID Token Signatures ED25519");
+
+        List<ResponseType> responseTypes = Arrays.asList(ResponseType.ID_TOKEN);
+
+        // 1. Registration
+        RegisterRequest registerRequest = new RegisterRequest(ApplicationType.WEB, "jans test app",
+                StringUtils.spaceSeparatedToList(redirectUris));
+        registerRequest.setResponseTypes(responseTypes);
+        registerRequest.setIdTokenSignedResponseAlg(SignatureAlgorithm.ED25519);
+        registerRequest.setSectorIdentifierUri(sectorIdentifierUri);
+
+        RegisterClient registerClient = new RegisterClient(registrationEndpoint);
+        registerClient.setRequest(registerRequest);
+        RegisterResponse registerResponse = registerClient.exec();
+
+        showClient(registerClient);
+        assertEquals(registerResponse.getStatus(), 201, "Unexpected response code: " + registerResponse.getEntity());
+        assertNotNull(registerResponse.getClientId());
+        assertNotNull(registerResponse.getClientSecret());
+        assertNotNull(registerResponse.getRegistrationAccessToken());
+        assertNotNull(registerResponse.getClientSecretExpiresAt());
+
+        String clientId = registerResponse.getClientId();
+
+        // 2. Request Authorization
+        List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
+        String nonce = UUID.randomUUID().toString();
+        String state = UUID.randomUUID().toString();
+
+        AuthorizationRequest authorizationRequest = new AuthorizationRequest(responseTypes, clientId, scopes, redirectUri, nonce);
+        authorizationRequest.setState(state);
+
+        AuthorizationResponse authorizationResponse = authenticateResourceOwnerAndGrantAccess(
+                authorizationEndpoint, authorizationRequest, userId, userSecret);
+
+        assertNotNull(authorizationResponse.getLocation());
+        assertNotNull(authorizationResponse.getIdToken());
+        assertNotNull(authorizationResponse.getState());
+
+        String idToken = authorizationResponse.getIdToken();
+
+        // 3. Validate id_token
+        Jwt jwt = Jwt.parse(idToken);
+        EDDSAPublicKey publicKey = JwkClient.getEDDSAPublicKey(
+                jwksUri,
+                jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+        
+        System.out.println("ed25519: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));        
+        
+        EDDSASigner ecdsaSigner = new EDDSASigner(SignatureAlgorithm.ED25519, publicKey);
+        assertTrue(ecdsaSigner.validate(jwt));
+    }
+    
+    @Parameters({"redirectUris", "userId", "userSecret", "redirectUri", "sectorIdentifierUri"})
+    @Test
+    public void usesAsymmetricIdTokenSignaturesED448(
+            final String redirectUris, final String userId, final String userSecret, final String redirectUri,
+            final String sectorIdentifierUri) throws Exception {
+        showTitle("OC5:FeatureTest-Uses Asymmetric ID Token Signatures ed448");
+
+        List<ResponseType> responseTypes = Arrays.asList(ResponseType.ID_TOKEN);
+
+        // 1. Registration
+        RegisterRequest registerRequest = new RegisterRequest(ApplicationType.WEB, "jans test app",
+                StringUtils.spaceSeparatedToList(redirectUris));
+        registerRequest.setResponseTypes(responseTypes);
+        registerRequest.setIdTokenSignedResponseAlg(SignatureAlgorithm.ED448);
+        registerRequest.setSectorIdentifierUri(sectorIdentifierUri);
+
+        RegisterClient registerClient = new RegisterClient(registrationEndpoint);
+        registerClient.setRequest(registerRequest);
+        RegisterResponse registerResponse = registerClient.exec();
+
+        showClient(registerClient);
+        assertEquals(registerResponse.getStatus(), 201, "Unexpected response code: " + registerResponse.getEntity());
+        assertNotNull(registerResponse.getClientId());
+        assertNotNull(registerResponse.getClientSecret());
+        assertNotNull(registerResponse.getRegistrationAccessToken());
+        assertNotNull(registerResponse.getClientSecretExpiresAt());
+
+        String clientId = registerResponse.getClientId();
+
+        // 2. Request Authorization
+        List<String> scopes = Arrays.asList("openid", "profile", "address", "email");
+        String nonce = UUID.randomUUID().toString();
+        String state = UUID.randomUUID().toString();
+
+        AuthorizationRequest authorizationRequest = new AuthorizationRequest(responseTypes, clientId, scopes, redirectUri, nonce);
+        authorizationRequest.setState(state);
+
+        AuthorizationResponse authorizationResponse = authenticateResourceOwnerAndGrantAccess(
+                authorizationEndpoint, authorizationRequest, userId, userSecret);
+
+        assertNotNull(authorizationResponse.getLocation());
+        assertNotNull(authorizationResponse.getIdToken());
+        assertNotNull(authorizationResponse.getState());
+
+        String idToken = authorizationResponse.getIdToken();
+
+        // 3. Validate id_token
+        Jwt jwt = Jwt.parse(idToken);
+        EDDSAPublicKey publicKey = JwkClient.getEDDSAPublicKey(
+                jwksUri,
+                jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));
+        
+        System.out.println("ed448: key id = " + jwt.getHeader().getClaimAsString(JwtHeaderName.KEY_ID));        
+        
+        EDDSASigner ecdsaSigner = new EDDSASigner(SignatureAlgorithm.ED448, publicKey);
+        assertTrue(ecdsaSigner.validate(jwt));
+    }
+    
 }
