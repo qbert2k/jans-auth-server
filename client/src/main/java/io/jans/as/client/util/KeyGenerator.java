f5aeae5f03024852642c0401e8f8a20f5d3a50d7
SMan <sman@smansoft.com>
2021-08-24 00:15:36 +0300

    updating install keys generator: adding new keys (EDDSA: 25519, 448, ECDSA: secp256k1): draft 01;

--- client/src/main/java/io/jans/as/client/util/KeyGenerator.java
+++ client/src/main/java/io/jans/as/client/util/KeyGenerator.java
@@ -51,7 +51,7 @@ import io.jans.util.StringHelper;
  * Command example:
  * java -cp bcprov-jdk15on-1.54.jar:.jar:bcpkix-jdk15on-1.54.jar:commons-cli-1.2.jar:commons-codec-1.5.jar:commons-lang-2.6.jar:jettison-1.3.jar:log4j-1.2.14.jar:oxauth-model.jar:oxauth.jar KeyGenerator -h
  * <p/>
- * KeyGenerator -sig_keys RS256 RS384 RS512 ES256 ES384 ES512 PS256 PS384 PS512 -enc_keys RSA_OAEP RSA1_5 -keystore /Users/JAVIER/tmp/mykeystore.jks -keypasswd secret -dnname "CN=Jans Auth CA Certificates" -expiration 365
+ * KeyGenerator -sig_keys RS256 RS384 RS512 ES256 ES256K ES384 ES512 PS256 PS384 PS512 Ed25519 Ed448 -enc_keys RSA_OAEP RSA1_5 -keystore /Users/JAVIER/tmp/mykeystore.jks -keypasswd secret -dnname "CN=Jans Auth CA Certificates" -expiration 365
  * <p/>
  * KeyGenerator -sig_keys RS256 RS384 RS512 ES256 ES384 ES512 -ox11 https://ce.gluu.info:8443/oxeleven/rest/generateKey -expiration 365 -at xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
  *
@@ -59,6 +59,7 @@ import io.jans.util.StringHelper;
  * @author Yuriy Movchan
  * @version February 12, 2019
  */
+@SuppressWarnings("deprecation")
 public class KeyGenerator {
 
     private static final String SIGNING_KEYS = "sig_keys";
@@ -90,7 +91,7 @@ public class KeyGenerator {
             this.args = args;
 
             Option signingKeysOption = new Option(SIGNING_KEYS, true,
-                    "Signature keys to generate. (RS256 RS384 RS512 ES256 ES384 ES512 PS256 PS384 PS512).");
+                    "Signature keys to generate. (RS256 RS384 RS512 ES256 ES256K ES384 ES512 PS256 PS384 PS512 Ed25519 Ed448).");
             signingKeysOption.setArgs(Option.UNLIMITED_VALUES);
 
             Option encryptionKeysOption = new Option(ENCRYPTION_KEYS, true,
